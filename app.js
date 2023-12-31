85be87aa213bdb0d703c70691d0ca46d7a09eec1
danielush <112973515+daniel-efrat@users.noreply.github.com>
2023-12-31 20:46:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const timerElement = document.getElementById("timer")
 const startBtn = document.getElementById("startBtn")
 const stopBtn = document.getElementById("stopBtn")
 const notificationSound = document.getElementById("notificationSound")
+const volumeSlider = document.getElementById("volumeSlider")
 
 function updateTimerDisplay() {
   const minutes = String(Math.floor(countdown / 60000)).padStart(2, "0")
@@ -12,20 +13,27 @@ function updateTimerDisplay() {
     2,
     "0"
   )
-  const milliseconds = String(countdown % 1000).padStart(3, "0")
+  const milliseconds = String(Math.floor((countdown % 1000) / 10)).padStart(
+    2,
+    "0"
+  ) // Showing only two digits of milliseconds
   timerElement.textContent = `${minutes}:${seconds}.${milliseconds}`
+}
+
+function playSound() {
+  notificationSound.volume = volumeSlider.value
+  notificationSound.currentTime = 0 // Reset audio playback to start
+  notificationSound.play()
 }
 
 function startTimer() {
   if (!timer) {
-    countdown = 300000 // Reset to 5 minutes in milliseconds
-    updateTimerDisplay()
-    notificationSound.play() // Play sound on start
+    playSound() // Play sound on start
     timer = setInterval(() => {
       countdown -= 10 // Decrease every 10 milliseconds
       updateTimerDisplay()
       if (countdown <= 0) {
-        notificationSound.play() // Play sound at the end of each interval
+        playSound() // Play sound at the end of each interval
         countdown = 300000 // Reset to 5 minutes
       }
     }, 10) // Update every 10 milliseconds
@@ -35,19 +43,15 @@ function startTimer() {
 function stopTimer() {
   clearInterval(timer)
   timer = null // Reset the timer variable
-  countdown = 300000
+  countdown = 300000 // Reset to 5 minutes
   updateTimerDisplay()
 }
 
 startBtn.addEventListener("click", startTimer)
 stopBtn.addEventListener("click", stopTimer)
 
-updateTimerDisplay()
-const volumeSlider = document.getElementById("volumeSlider")
-
-function updateVolume() {
+volumeSlider.addEventListener("input", () => {
   notificationSound.volume = volumeSlider.value
-  notificationSound.play()
-}
+})
 
-volumeSlider.addEventListener("input", updateVolume)
+updateTimerDisplay()
